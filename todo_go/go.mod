a8572bdc2f7f020a5bddba17707af85fc3c2ab60
letmebelazy <letmesleepmore@gmail.com>
2021-07-31 03:13:07 +0900

    ver.2 complete

--- todo_go/go.mod
+++ todo_go/go.mod
@@ -2,4 +2,7 @@ module todo_flutter_go_ver2/todo_go
 
 go 1.13
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
+)
--- todo_go/server.go
+++ todo_go/server.go
@@ -8,16 +8,16 @@ import (
 	"log"
 	"net/http"
 
-	_ "github.com/go-sql-driver-mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 const port = ":5500"
 
 type Todo struct {
-	Index int
-	Name  string
-	Title string
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Title string `json:"title"`
 }
 
 var db *sql.DB
@@ -34,10 +34,10 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", getToRootPage)
 	router.HandleFunc("/todos", getAllTodos).Methods("GET")
-	router.HandleFunc("/todos/${index}", getTodo).Methods("GET")
+	router.HandleFunc("/todos/{id}", getTodo).Methods("GET")
 	router.HandleFunc("/todos", createTodo).Methods("POST")
-	router.HandleFunc("/todos/${index}", updateTodo).Methods("PATCH")
-	router.HandleFunc("/todos/${index}", deleteTodo).Methods("DELETE")
+	router.HandleFunc("/todos/{id}", updateTodo).Methods("PATCH")
+	router.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
 
 	fmt.Println("Serving @ http://127.0.0.1" + port)
 	log.Fatal(http.ListenAndServe(port, router))
@@ -52,7 +52,7 @@ func getAllTodos(w http.ResponseWriter, r *http.Request) {
 
 	var todos []Todo
 
-	result, err := db.Query("SELECT index, name, title from todos")
+	result, err := db.Query("SELECT * FROM todos")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -61,7 +61,7 @@ func getAllTodos(w http.ResponseWriter, r *http.Request) {
 
 	for result.Next() {
 		var todo Todo
-		err := result.Scan(&todo.Index, &todo.Name, &todo.Title)
+		err := result.Scan(&todo.Id, &todo.Name, &todo.Title)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -75,8 +75,7 @@ func getTodo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
-
-	result, err := db.Query("SELECT index, name, title FROM todos WHERE index = ?", params["id"])
+	result, err := db.Query("SELECT * FROM todos WHERE id = ?", params["id"])
 	if err != nil {
 		panic(err.Error())
 	}
@@ -86,7 +85,7 @@ func getTodo(w http.ResponseWriter, r *http.Request) {
 	var todo Todo
 
 	for result.Next() {
-		err := result.Scan(&todo.Index, &todo.Name, &todo.Title)
+		err := result.Scan(&todo.Id, &todo.Name, &todo.Title)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -120,9 +119,43 @@ func createTodo(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateTodo(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	stmt, err := db.Prepare("UPDATE todos SET name = ?, title = ? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	todoMap := make(map[string]string)
+	json.Unmarshal(body, &todoMap)
+	name := todoMap["name"]
+	title := todoMap["title"]
+
+	_, err = stmt.Exec(name, title, params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Todo with id = %s was updated", params["id"])
 }
 
 func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	stmt, err := db.Prepare("DELETE FROM todos WHERE id =?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Todo with id = %s was deleted", params["id"])
 }
